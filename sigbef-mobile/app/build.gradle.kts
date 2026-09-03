368147a76d7564c8a0d526100554f4545f2650f7
Marcello Melo de Medeiros Costa <uemastelo@gmail.com>
2026-09-03 20:54:02 -0300

    chore: versão 1.12.0

--- sigbef-mobile/app/build.gradle.kts
+++ sigbef-mobile/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "br.rn.cefe.sigbef"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
-    versionName = "0.2"
+    versionCode = 3
+    versionName = "0.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
